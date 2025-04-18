c4bfa671895191eed359c8b5f39be3b94ceda0c1
YuivonBock <b.bolor0409@gmail.com>
2025-04-18 13:48:12 +0800

    3: Sprites, Draw Player

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #Creating an instance of Player
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,8 +23,11 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    
+
+        # Limiting the FPS to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
